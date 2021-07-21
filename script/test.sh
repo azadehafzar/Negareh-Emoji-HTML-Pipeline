80b5047f61be35f1279f3dffd4ade2897c38bc05
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2021-07-21 19:05:54 +0430

    modify: explicitly define files for tests

--- script/test.sh
+++ script/test.sh
@@ -6,7 +6,14 @@
 # set flag for shell execution.
 # -e  Exit immediately if a command exits with a non-zero status.
 # -x  Print commands and their arguments as they are executed.
-set -ex
+set -e
 
-script/test_style.sh
-# script/test_module.sh # TODO write tests
+# files for testing.
+NEGAR_PIP_RB="../lib/html/pipeline/negarmoji-pipeline.rb"
+FILTER_RB="../lib/html/pipeline/negarmoji-pipeline/filter.rb"
+VERSION_RB="../lib/html/pipeline/negarmoji-pipeline/version.rb"
+
+./test_style.sh "${NEGAR_PIP_RB}" "${FILTER_RB}" "${VERSION_RB}"
+
+# TODO write tests first :)
+# ./test_module.sh "${NEGAR_PIP_RB}" "${FILTER_RB}" "${VERSION_RB}"
